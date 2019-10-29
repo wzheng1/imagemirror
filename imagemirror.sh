30d2d09d1c0d3a376056ceab82745676f16a7cc9
Wenjing Zheng <wzheng@redhat.com>
2019-10-29 16:19:51 +0800

    Update imagemirror.sh

--- imagemirror.sh
+++ imagemirror.sh
@@ -8,8 +8,7 @@ do
   imagename=`echo "$line"| awk -F"/" '{print $3}'`
   oc image mirror $line $MIRROR_REGISTRY/$namespace/$imagename
   if [ $? -ne 0 ]; then
-    echo TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")  >> $RESULT_FILE
-    echo -e "${item} image mirrors failed, please try it again.\n#oc image mirror registry.redhat.io/rhscl/${item} ${MIRROR_REGISTRY}/rhscl/${item}" >> $RESULT_FILE
+    echo -e "Image mirrors failed, please try it again.\n"
 done < "$input"
 
 echo "Mirror images for jenkins module.... "
@@ -19,7 +18,6 @@ for item in ${listjenkins[@]}
 do
   oc image mirror quay.io/openshift-release-dev/$item ${MIRROR_REGISTRY}/openshift-release-dev/ocp-v4.0-art-dev
   if [ $? -ne 0 ]; then
-    echo TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")  >> $RESULT_FILE
-    echo -e "${item} image mirrors failed, please try it again.\n#oc image mirror quay.io/openshift-release-dev/$item ${MIRROR_REGISTRY}/openshift-release-dev/ocp-v4.0-art-dev" >>$RESULT_FILE
+    echo -e "Image mirrors failed, please try it again.\n"
   fi
 done
