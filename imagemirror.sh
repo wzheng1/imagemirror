cb2c4ffac5bf0295dd9bc6df52ff48ee81c4718f
wzheng <wzheng@openshift-qe.desktop.redhat.cn>
2019-10-29 17:40:22 +0800

    p

--- imagemirror.sh
+++ imagemirror.sh
@@ -8,7 +8,7 @@ while IFS= read -r line
 do
   namespace=`echo "$line"| awk -F"/" '{print $2}'`
   imagename=`echo "$line"| awk -F"/" '{print $3}'`
-  oc image mirror $line $MIRROR_REGISTRY/$namespace/$imagename
+  oc image mirror -a #{File.expand_path('~/mirror_pullsecret_config.json') $line $MIRROR_REGISTRY/$namespace/$imagename
   if [ $? -ne 0 ]; then
     echo -e "Image mirrors failed, please try it again.\n"
   fi
@@ -19,7 +19,7 @@ VERSION=`oc get clusterversion | grep "version" | awk '{print $2}'`
 listjenkins=`oc adm release info --pullspecs registry.svc.ci.openshift.org/ocp/release:$VERSION | grep  jenkins| awk -F"/" '{print $3}'`
 for item in ${listjenkins[@]}
 do
-  oc image mirror quay.io/openshift-release-dev/$item ${MIRROR_REGISTRY}/openshift-release-dev/ocp-v4.0-art-dev
+  oc image mirror -a #{File.expand_path('~/mirror_pullsecret_config.json') quay.io/openshift-release-dev/$item ${MIRROR_REGISTRY}/openshift-release-dev/ocp-v4.0-art-dev
   if [ $? -ne 0 ]; then
     echo -e "Image mirrors failed, please try it again.\n"
   fi
